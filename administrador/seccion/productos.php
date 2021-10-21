f4525cc7057f96720c4bedbf64771e5f65fd67bd
Norberto Aguilar Ramirez <norchilipeper@hotmail.com>
2021-10-21 12:19:20 -0600

    Primer commit: Sitio web con php,MySQL, bootstrap

---
+++ administrador/seccion/productos.php
@@ -0,0 +1,11 @@
+<?php include("../template/cabecera.php"); ?>
+
+<div class="col-md-5">
+    Formulario de agregar libros.
+</div>
+
+<div class="col-md-7">
+    Tabla de libros (mostrar los datos de los libros)
+</div>
+
+<?php include("../template/pie.php"); ?>
---
+++ productos.php
@@ -0,0 +1,56 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://avatars.dicebear.com/v2/avataaars/767daec97dabae3bd0d3c722d68073dd.svg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver mas </a>
+        </div>
+    </div>
+    
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://avatars.dicebear.com/v2/avataaars/767daec97dabae3bd0d3c722d68073dd.svg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver mas </a>
+        </div>
+    </div>
+    
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://avatars.dicebear.com/v2/avataaars/767daec97dabae3bd0d3c722d68073dd.svg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver mas </a>
+        </div>
+    </div>
+    
+</div>
+
+<div class="col-md-3">
+
+    <div class="card">
+        <img class="card-img-top" src="https://avatars.dicebear.com/v2/avataaars/767daec97dabae3bd0d3c722d68073dd.svg" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro Php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button"> Ver mas </a>
+        </div>
+    </div>
+    
+</div>
+
+
+
+
+
+
+<?php include("template/pie.php"); ?>
