945ebddcff6bc8a93d556ac769c8907b64a10039
Norberto Aguilar Ramirez <norchilipeper@hotmail.com>
2021-10-22 12:49:51 -0600

    Se completo las opciones de modificar, borrar, seleccionar, agregar de Productos

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -15,12 +15,54 @@ switch($accion){
     case "Agregar":
         $sentenciaSQL= $conexion->prepare("INSERT INTO libros (nombre,imagen) VALUES (:nombre,:imagen);");
         $sentenciaSQL->bindParam(':nombre',$txtNombre); //Parametro a reemplazar
-        $sentenciaSQL->bindParam(':imagen',$imagen); //Parametro a reemplazar
+        
+        $fecha= new DateTime();
+        //Si txtImagen es diferente de vacio agregamos fecha y concatenamos _ y concatenamos nombre del archivo imagen
+        //Si no, solo agregamos como nombre imagen.jpg
+        $nombreArchivo=($imagen!="")? $fecha->getTimestamp()."_".$_FILES["imagen"]["name"]:"imagen.jpg" ;
+        $tmpImagen=$_FILES["imagen"]["tmp_name"];
+        
+        if($tmpImagen!=""){
+            move_uploaded_file($tmpImagen,"../../img/".$nombreArchivo);
+        }
+        $sentenciaSQL->bindParam(':imagen',$nombreArchivo); //Parametro a reemplazar
         $sentenciaSQL->execute();
         break;
 
     case "Modificar":
-        echo "Presionado boton Modificar";
+
+
+        $sentenciaSQL= $conexion->prepare("UPDATE libros SET nombre=:nombre WHERE id=:id;");
+        $sentenciaSQL->bindParam(':nombre',$txtNombre);
+        $sentenciaSQL->bindParam(':id',$txtID);
+        $sentenciaSQL->execute();
+
+        if($imagen!=""){
+
+            $fecha= new DateTime();
+            $nombreArchivo=($imagen!="") ? $fecha->getTimestamp()."_".$_FILES["imagen"]["name"] : "imagen.jpg" ;//renombra el archivo imagen
+            $tmpImagen=$_FILES["imagen"]["tmp_name"];
+            move_uploaded_file($tmpImagen,"../../img/".$nombreArchivo);//sube la imagen nueva a la carptea img
+
+            //Seleccionamos la imagen de la Base de datos de acuerdo al id
+            $sentenciaSQL= $conexion->prepare("SELECT imagen FROM libros WHERE id=:id;");
+            $sentenciaSQL->bindParam(':id',$txtID);
+            $sentenciaSQL->execute();
+            $libro=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
+
+            //borramos la imagen de la carpeta IMG de acuerdo a la imagen seleccionada de la Base de Datos
+            if(isset($libro["imagen"]) && $libro["imagen"]!="imagen.jpg"){
+                if(file_exists("../../img/".$libro["imagen"])){
+                    unlink("../../img/".$libro["imagen"]);
+                }
+            }
+            
+            //Actualizamos la Base de datos con el nombre de la imagen nueva
+            $sentenciaSQL= $conexion->prepare("UPDATE libros SET imagen=:imagen WHERE id=:id;");
+            $sentenciaSQL->bindParam(':imagen',$nombreArchivo);
+            $sentenciaSQL->bindParam(':id',$txtID);
+            $sentenciaSQL->execute();
+        }
         break; 
 
     case "Cancelar":
@@ -28,14 +70,31 @@ switch($accion){
         break;
 
     case "Seleccionar":
-        echo "Presionado boton Seleccionar";
+        $sentenciaSQL= $conexion->prepare("SELECT * FROM libros WHERE id=:id;");
+        $sentenciaSQL->bindParam(':id',$txtID);
+        $sentenciaSQL->execute();
+        $libro=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
+        $txtNombre=$libro['nombre'];
+        $imagen=$libro['imagen'];
         break;
 
     case "Borrar":
+        $sentenciaSQL= $conexion->prepare("SELECT imagen FROM libros WHERE id=:id;");
+        $sentenciaSQL->bindParam(':id',$txtID);
+        $sentenciaSQL->execute();
+        $libro=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
+
+        if(isset($libro["imagen"]) && $libro["imagen"]!="imagen.jpg"){
+            if(file_exists("../../img/".$libro["imagen"])){
+                unlink("../../img/".$libro["imagen"]);
+            }
+        }
+
+        
         $sentenciaSQL= $conexion->prepare("DELETE FROM libros WHERE id=:id;");
         $sentenciaSQL->bindParam(':id',$txtID);
         $sentenciaSQL->execute();
-        echo "Presionado boton Borrar";
+        
         break;
 }
 
@@ -58,16 +117,23 @@ $listaLibros=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
 
                 <div class = "form-group">
                     <label for="txtID">ID:</label>
-                    <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
+                    <input type="text" class="form-control" value="<?php echo $txtID;?>" name="txtID" id="txtID" placeholder="ID">
                 </div>
 
                 <div class = "form-group">
                     <label for="txtNombre">Nombre:</label>
-                    <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre">
+                    <input type="text" class="form-control" value="<?php echo $txtNombre;?>" name="txtNombre" id="txtNombre" placeholder="Nombre">
                 </div>
 
                 <div class = "form-group">
                     <label for="imagen">Imagen:</label>
+                    <br/>
+                    <?php if($imagen!=""){?>
+
+                        <img class="img-thumbnail rounded" src="../../img/<?php echo $imagen;?>" width="50" alt="">
+
+                    <?php }?>
+                    
                     <input type="file" class="form-control" name="imagen" id="imagen" placeholder="Imagen">
                 </div>
 
@@ -104,7 +170,10 @@ $listaLibros=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
             <tr>
                 <td><?php echo $libro['id'];?></td>
                 <td><?php echo $libro['nombre'];?></td>
-                <td><?php echo $libro['imagen'];?></td>
+                <td>
+                    
+                    <img class="img-thumbnail rounded" src="../../img/<?php echo $libro['imagen'];?>" width="50" alt="">
+                </td>
                 <td>
                     Seleccionar | Borrar
                     <form method="post">
